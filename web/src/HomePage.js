ccc9c21b4f22be97aaea4a93cf440f7e04896912
Anish Aggarwal <anish17122000@gmail.com>
2020-03-04 01:06:34 +0530

    added loader to home screen

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import * as Backend from "./Backend";
 import AdapterTable from "./AdapterTable";
-import {Button, Card, Col, Row} from "antd";
+import { Button, Card, Col, Row } from "antd";
 import * as Setting from "./Setting";
 import ModelTable from "./ModelTable";
 import EnforcerTable from "./EnforcerTable";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -14,6 +16,7 @@ class HomePage extends React.Component {
       adapters: null,
       models: null,
       enforcers: null,
+      loading: true,
     };
   }
 
@@ -25,63 +28,78 @@ class HomePage extends React.Component {
 
   getAdapters() {
     Backend.getAdapters()
-      .then((res) => {
-          this.setState({
-            adapters: res,
-          });
-        }
-      );
+      .then(res => {
+        this.setState({
+          adapters: res,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
   }
 
   getModels() {
     Backend.getModels()
-      .then((res) => {
-          this.setState({
-            models: res,
-          });
-        }
-      );
+      .then(res => {
+        this.setState({
+          models: res,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
   }
 
   getEnforcers() {
     Backend.getEnforcers()
-      .then((res) => {
-          this.setState({
-            enforcers: res,
-          });
-        }
-      );
+      .then(res => {
+        this.setState({
+          enforcers: res,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
   }
 
   onUpdateAdapters(adapters) {
     this.setState({
       adapters: adapters,
+      loading: false,
     });
   }
 
   onUpdateModels(models) {
     this.setState({
       models: models,
+      loading: false,
     });
   }
 
   onUpdateEnforcers(enforcers) {
     this.setState({
       enforcers: enforcers,
+      loading: false,
     });
   }
 
   updateMetadata() {
     Backend.updateAdapters(this.state.adapters)
-      .then((res) => {
+      .then(res => {
         // Setting.showMessage("success", `Save succeeded`);
 
         Backend.updateModels(this.state.models)
-          .then((res) => {
+          .then(res => {
             // Setting.showMessage("success", `Save succeeded`);
 
             Backend.updateEnforcers(this.state.enforcers)
-              .then((res) => {
+              .then(res => {
                 Setting.showMessage("success", `Save succeeded`);
               })
               .catch(error => {
@@ -99,52 +117,81 @@ class HomePage extends React.Component {
 
   renderContent() {
     return (
-      <Card size="small" title={
-        <div style={{width: "90vw"}}>
-          Edit Metadata&nbsp;&nbsp;&nbsp;&nbsp;
-          <Button type="primary" onClick={this.updateMetadata.bind(this)}>Save Change</Button>
-        </div>
-      } style={{marginLeft: '5px'}} type="inner">
-        <Row style={{marginTop: '20px'}} >
-          <Col style={{marginTop: '5px'}} span={2}>
+      <Card
+        size="small"
+        title={
+          <div style={{ width: "90vw" }}>
+            Edit Metadata&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button type="primary" onClick={this.updateMetadata.bind(this)}>
+              Save Change
+            </Button>
+          </div>
+        }
+        style={{ marginLeft: "5px" }}
+        type="inner"
+      >
+        <Row style={{ marginTop: "20px" }}>
+          <Col style={{ marginTop: "5px" }} span={2}>
             Models:
           </Col>
-          <Col span={22} >
-            <ModelTable title="Models" table={this.state.models} onUpdateTable={this.onUpdateModels.bind(this)} />
+          <Col span={22}>
+            <ModelTable
+              title="Models"
+              table={this.state.models}
+              onUpdateTable={this.onUpdateModels.bind(this)}
+            />
           </Col>
         </Row>
-        <Row style={{marginTop: '20px'}} >
-          <Col style={{marginTop: '5px'}} span={2}>
+        <Row style={{ marginTop: "20px" }}>
+          <Col style={{ marginTop: "5px" }} span={2}>
             Adapters:
           </Col>
-          <Col span={22} >
-            <AdapterTable title="Adapters" table={this.state.adapters} onUpdateTable={this.onUpdateAdapters.bind(this)} />
+          <Col span={22}>
+            <AdapterTable
+              title="Adapters"
+              table={this.state.adapters}
+              onUpdateTable={this.onUpdateAdapters.bind(this)}
+            />
           </Col>
         </Row>
-        <Row style={{marginTop: '20px'}} >
-          <Col style={{marginTop: '5px'}} span={2}>
+        <Row style={{ marginTop: "20px" }}>
+          <Col style={{ marginTop: "5px" }} span={2}>
             Enforcers:
           </Col>
-          <Col span={22} >
-            <EnforcerTable title="Enforcers" table={this.state.enforcers} models={this.state.models === null ? [] : this.state.models} adapters={this.state.adapters === null ? [] : this.state.adapters} onUpdateTable={this.onUpdateEnforcers.bind(this)} />
+          <Col span={22}>
+            <EnforcerTable
+              title="Enforcers"
+              table={this.state.enforcers}
+              models={this.state.models === null ? [] : this.state.models}
+              adapters={this.state.adapters === null ? [] : this.state.adapters}
+              onUpdateTable={this.onUpdateEnforcers.bind(this)}
+            />
           </Col>
         </Row>
       </Card>
-    )
-  }
-
-  render() {
-    return (
-      <div>
-        <Row>
-          {
-            this.state.adapters !== null ? this.renderContent() : null
-          }
-        </Row>
-      </div>
     );
   }
 
+  render() {
+    const { loading } = this.state;
+    if (loading) {
+      return (
+        <Loader
+          type="TailSpin"
+          color="#61dafb"
+          height={110}
+          width={110}
+          visible={true}
+          className="loader"
+        />
+      );
+    }
+    return (
+      <div>
+        <Row>{this.state.adapters !== null ? this.renderContent() : null}</Row>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
