fe4b5ededf41eb1991a6fd33f4555e49bd23f616
Kartik Singh <kssklol925@gmail.com>
2020-12-14 00:52:28 +0530

    fixed some typos and pointer

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,29 +3,4 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 'off',
   },
-  env: {
-    commonjs: true,
-    node: true,
-    browser: true,
-    es6: true,
-    jest: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  globals: {},
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'import', 'react-hooks'],
-  ignorePatterns: ['node_modules/'],
-  rules: {},
-  settings: {
-    react: {
-      version: 'latest', // "detect" automatically picks the version you have installed.
-    },
-  },
 };
